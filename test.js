49b026d0ea371fb48569e3f0deddb7d692b4441a
HaoAlone <1216787948@qq.com>
2021-05-05 22:26:56 +0800

    e

--- test.js
+++ test.js
@@ -14,3 +14,23 @@ console.log(person instanceof Object) // true
 
 console.log(typeof PersonClass) // function
 console.log(typeof PersonClass.prototype.sayName) // function
+
+let obj = {
+    name:'王刚',
+    age:22,
+    s:''
+}
+
+let data = 12
+Object.defineProperty(obj,'s',{
+    get:function(){
+        console.log('触发get')
+        return data
+    },
+  set:function(newVal){
+      console.log('触发set')
+      data = newVal
+      console.log('obj:'+obj)
+  }
+})
+a = obj.s
